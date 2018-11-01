7dceb72f134105d52172db6740053053d61d4e2b
Daisho <daishokomiyama@gmail.com>
2018-10-31 21:54:30 -0400

    Adding flash message

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
+const Review = mongoose.model('Review');
+
 exports.homePage = async (req, res) => {
     res.render('reviews', { title: 'Reviews' });
 };
 
 exports.addReview = (req, res) => {
     res.render('editReview', { title: 'Add Review' });
+};
+
+exports.createReview = async (req, res) => {
+    const review = new Review(req.body);
+    await review.save();
+    req.flash('success', `Successfully created ${review.course}!`);
+    res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', reviewController.homePage);
 router.get('/reviews', reviewController.homePage);
 
 router.get('/add', reviewController.addReview);
+router.post('/add', catchErrors(reviewController.createReview));
 
 module.exports = router;
