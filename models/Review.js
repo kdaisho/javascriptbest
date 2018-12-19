f61dd5e71368711c2163c11eb24a8c0ce51667d0
Daisho <daishokomiyama@gmail.com>
2018-12-18 22:08:23 -0500

    Adding functionality avoids to have duplicated slugs

--- models/Review.js
+++ models/Review.js
@@ -31,12 +31,18 @@ const reviewSchema = new mongoose.Schema({
     image: String
 });
 
-reviewSchema.pre('save', function(next) {
+reviewSchema.pre('save', async function(next) {
     if (!this.isModified('course')) {
         next();
         return;
     }
     this.slug = slug(this.course);
+    // Find other reviews that have a slug of example, example-1, example-2
+    const slugRegExp = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const reviewsWithSlug = await this.constructor.find({ slug: slugRegExp });
+    if (reviewsWithSlug.length) {
+        this.slug = `${this.slug}-${reviewsWithSlug.length + 1}`;
+    }
     next();
     // TODO make more resiliant so slugs are unique
 });
