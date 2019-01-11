30e7075a404438c6e72dc564085e6ae3e07a08e8
Daisho <daishokomiyama@gmail.com>
2019-01-10 22:46:54 -0500

    Adding schema for password reset flow

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
     failureRedirect: '/login',
@@ -19,5 +22,23 @@ exports.isLoggedIn = (req, res, next) => {
         return;
     }
     req.flash('error', 'Oops! You must be logged in to do that!');
+    res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+    // 1. See if the user with that email exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        req.flash('error', 'No account with that email exists.');
+        return res.redirect('/login');
+    }
+    // 2. Set reset tokens and expiry
+    user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+    await user.save();
+    // 3. Send them an email with the token
+    const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+    req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
+    // 4. Redirect to login page
     res.redirect('/login');
 };
--- routes/index.js
+++ routes/index.js
@@ -43,5 +43,6 @@ router.post('/signup',
 router.get('/logout', authController.logout);
 router.get('/account', authController.isLoggedIn, userController.account);
 router.post('/account', catchErrors(userController.updateAccount));
+router.post('/account/forgot', catchErrors(authController.forgot));
 
 module.exports = router;
