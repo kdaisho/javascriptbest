4e7eb09d6988b98bc262be8b4a173971ac8ad072
Daisho <daishokomiyama@gmail.com>
2019-01-28 22:31:23 -0500

    Adding search interface

--- public/js/modules/typeAhead.js
+++ public/js/modules/typeAhead.js
@@ -1,7 +1,41 @@
 const axios = require('axios');
 
+function searchResultsHTML(reviews) {
+    return reviews.map(review => {
+        return `
+            <a href="/review/${review.slug}" class="search-result">
+                <strong>${review.course}</strong>
+            </a>
+        `;
+    }).join('');
+}
+
 function typeAhead(search) {
-    console.log('ser', search);
+    if (!search) return;
+
+    const searchInput = search.querySelector('input[name="search"]');
+    const searchResults = search.querySelector('.search-results');
+
+    searchInput.on('input', function() {
+        if (!this.value) {
+            searchResults.style.display = 'none';
+            return;
+        }
+
+        searchResults.style.display = 'block';
+        searchResults.innerHTML = '';
+
+        axios
+            .get(`/api/search?q=${this.value}`)
+            .then(res => {
+                if (res.data.length) {
+                    searchResults.innerHTML = searchResultsHTML(res.data);
+                }
+            })
+            .catch(err => {
+                console.log(error(err));
+            });
+    });
 }
 
 export default typeAhead;
