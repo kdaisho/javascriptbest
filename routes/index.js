5d0b8dbaa270c51749ffa0f11a44dc6b8772980c
Daisho <daishokomiyama@gmail.com>
2019-04-18 07:00:25 -0400

    Adding signup view

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,14 @@ router.get('/', (req, res) => { res.redirect('/courses') });
 router.get('/courses', catchErrors(courseController.getCourses));
 router.get('/courses/page/:page', catchErrors(courseController.getCourses));
 
+
+router.get('/signup', userController.signupForm);
+router.post('/signup',
+    userController.validateRegister,
+    userController.register,
+    authController.login
+);
+
 router.get('/login', userController.loginForm);
 router.post('/login', authController.login);
 
