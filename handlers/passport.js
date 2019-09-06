bdbfb0883ac97ecd8dee9390822fa7118621e55b
Daisho <daishokomiyama@gmail.com>
2019-09-05 22:28:58 -0400

    Adding google oath and callbackURL as an option for google strategy

--- handlers/passport.js
+++ handlers/passport.js
@@ -1,8 +1,21 @@
 const passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+console.log(GoogleStrategy);
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
+passport.use(
+    new GoogleStrategy({
+        // options for the google strat
+        consumerKey: process.env.clientId,
+        consumerSecret: process.env.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, () => {
+        // passport callback
+    })
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
