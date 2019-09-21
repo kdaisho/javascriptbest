0ec1ce261fdabddca037976897d83cf567440b77
Daisho <daishokomiyama@gmail.com>
2019-09-21 11:58:38 -0400

    Adding 404 page

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -19,9 +19,7 @@ exports.catchErrors = (fn) => {
 	If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
 */
 exports.notFound = (req, res, next) => {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+    res.render('404', { title: "Humm... Weird." });
 };
 
 
