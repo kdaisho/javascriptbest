a15394d98182bb44b93ffc452f094200ec5fa0a7
kdaisho <daishokomiyama@gmail.com>
2019-10-01 07:15:13 -0400

    Fixing typo

--- helpers.js
+++ helpers.js
@@ -43,6 +43,6 @@ exports.repeat = (item, times, total) => {
 };
 
 exports.arrayFive = [5, 4, 3, 2, 1];
-exports.technologies = ['Vanilla', 'Node', 'React', 'Angular', 'Vue', 'Library', 'Framework', 'Functional Programming', 'Classical Programming', 'TypeScript', 'PWA', 'Template Engine', 'Visualization', 'Machine Learning', 'Game', 'Database', 'Test-Driven', 'Fun', 'Beginner Friedly', 'intermediate', 'Advanced', 'Paid Course'];
+exports.technologies = ['Vanilla', 'Node', 'React', 'Angular', 'Vue', 'Library', 'Framework', 'Functional Programming', 'Classical Programming', 'TypeScript', 'PWA', 'Template Engine', 'Visualization', 'Machine Learning', 'Game', 'Database', 'Test-Driven', 'Fun', 'Beginner Friendly', 'intermediate', 'Advanced', 'Paid Course'];
 
 exports.siteName = 'JavaScriptBest';
